eab78178bf615236565d9ece91bf90a9f160b9cb
darkshan1234 <149415238+darkshan1234@users.noreply.github.com>
2024-03-09 10:59:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
-  SESSION_NAME: process.env.SESSION_NAME || "Bot~"
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://KUSHAN:4000@cluster0.zji09ho.mongodb.net/",
+  SESSION_NAME: process.env.SESSION_NAME || "DARKSHAN;;;"
 };
